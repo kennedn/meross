7ed561e4cc54ba74b5ae9a70c3a8fb750d65af0b
kennedn <kennedn@msn.com>
2025-02-25 14:49:32 +0000

    gisdiafg

--- tools/thermo_macros.sh
+++ tools/thermo_macros.sh
@@ -1,7 +1,7 @@
 bthome_status() {
-  curl -sX POST "https://api.kennedn.com/v2/radiator?hosts=kitchen,livingroom_back,livingroom_front,office&code=status" | jq -r "([\"NAME\",\"ONOFF\",\"MODE\",\"CURRENT\",\"TARGET\"] | @tsv),(.data[] | [.name,.status.onoff,.status.mode,.status.temperature.current,.status.temperature.target] | @tsv)" | column -t
+  curl -sX POST "https://api.kennedn.com/v2/bthome?hosts=office,bedroom,livingroom,kitchen&code=status" | jq -r "[(.data.devices[] | .status = {name: (.name | ascii_upcase)} + .status)] | (.[0].status | keys_unsorted | map(ascii_upcase) | @tsv),(.[] | [.status[]] | @tsv)" | column -t
 }
 radiator_status() {
-  curl -sX POST "https://api.kennedn.com/v2/bthome?hosts=office,bedroom,livingroom,kitchen&code=status" | jq -r "[(.data.devices[] | .status = {name: (.name | ascii_upcase)} + .status)] | (.[0].status | keys_unsorted | map(ascii_upcase) | @tsv),(.[] | [.status[]] | @tsv)" | column -t
+  curl -sX POST "https://api.kennedn.com/v2/radiator?hosts=kitchen,livingroom,office&code=status" | jq -r "([\"NAME\",\"ID\",\"ONOFF\",\"MODE\",\"CURRENT\",\"TARGET\"] | @tsv),(.data[] | [.name,.status.id,.status.onoff,.status.mode,.status.temperature.current,.status.temperature.target] | @tsv)" | column -t
 }
 
