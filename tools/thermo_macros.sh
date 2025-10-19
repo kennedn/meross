490c0a1774bcc1f75d38ac917727df52cb9f8b85
kennedn <kennedn@msn.com>
2025-10-19 12:13:03 +0100

    thermos_macros_bump

--- tools/thermo_macros.sh
+++ tools/thermo_macros.sh
@@ -5,3 +5,38 @@ radiator_status() {
   curl -sX POST "https://api.kennedn.com/v2/radiator?hosts=kitchen,livingroom,office&code=status" | jq -r "([\"NAME\",\"ID\",\"ONOFF\",\"MODE\",\"CURRENT\",\"TARGET\"] | @tsv),(.data[] | [.name,.status.id,.status.onoff,.status.mode,.status.temperature.current,.status.temperature.target] | @tsv)" | column -t
 }
 
+radiator_battery() {
+    curl -sX POST "https://api.kennedn.com/v2/radiator?hosts=kitchen,livingroom,office&code=battery" | jq -r "([\"NAME\",\"ID\",\"BATTERY(%)\"] | @tsv),(.data[] | [.name,.status.id,.status.value] | @tsv)" | column -t
+}
+
+radiator_schedule() {
+    jq -r '
+      (["NAME","DAY","ID","START","END","TEMPERATURE(°C)"] | @tsv),
+      (
+        {
+          "03000BDF":"OFFICE",
+          "0300B980":"LIVING1",
+          "0300B1F7":"LIVING2",
+          "0300CA57":"KITCHEN"
+        } as $device_names
+        |
+        [
+          .schedule[] as $sched
+          | ["mon","tue","wed","thu","fri","sat","sun"][] as $day
+          | ($sched[$day] // []) as $arr
+          | foreach $arr[] as $it
+              (0;
+               . + ($it[0] * 60);
+               {
+                 name: $device_names[$sched.id],
+                 day:  ($day | ascii_upcase),
+                 id: $sched.id,
+                 start: (. - ($it[0] * 60) | strftime("%H:%M")),
+                 end:   (. | strftime("%H:%M")),
+                 temp:  ($it[1] / 10)
+               })
+        ]
+        | map([.name, .day, .id, .start, .end, (.temp|tostring)] | @tsv)[]
+      )
+    ' | column -t
+}
